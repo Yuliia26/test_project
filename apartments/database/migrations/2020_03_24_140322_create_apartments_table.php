a0796a7fdef80153a9b80eae4f6d83396c3f11a2
Yuliia <yuliia.bublyk@gmail.com>
2020-03-24 16:14:45 +0200

    created database

---
+++ apartments/database/migrations/2020_03_24_140322_create_apartments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 25);
+            $table->integer('price');
+            $table->tinyInteger('bedrooms');
+            $table->tinyInteger('bathrooms');
+            $table->tinyInteger('storeys');
+            $table->tinyInteger('garages');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
